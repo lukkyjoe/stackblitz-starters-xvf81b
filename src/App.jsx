59ead21e2cb38cfd86bf3db34822e341a4e55ddd
lukkyjoe <joseph.sc.luk@gmail.com>
2023-11-01 09:42:50 -0700

    Add map

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/navbar.jsx';
+import Map from './components/map.jsx';
 
 function App() {
   return (
     <div className="App">
         <Navbar/>
+        <Map/>        
     </div>
   );
 }
--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,3 +1,6 @@
+
+import dotenv from 'dotenv'
+dotenv.config()
 import React, { useRef, useEffect, useState } from 'react';
 import * as maptilersdk from '@maptiler/sdk';
 import "@maptiler/sdk/dist/maptiler-sdk.css";
@@ -8,5 +11,21 @@ export default function Map() {
     const map = useRef(null);
     const tokyo = { lng: 139.753, lat: 35.6844 };
     const [zoom] = useState(14);
-    maptilersdk.config.apiKey = process.env.MAPTILER_API_KEY;    
+    maptilersdk.config.apiKey = process.env.MAPTILER_API_KEY;
+    useEffect(() => {
+        if (map.current) return; // stops map from intializing more than once
+      
+        map.current = new maptilersdk.Map({
+          container: mapContainer.current,
+          style: maptilersdk.MapStyle.STREETS,
+          center: [tokyo.lng, tokyo.lat],
+          zoom: zoom
+        });
+      
+      }, [tokyo.lng, tokyo.lat, zoom]);
+    return (
+    <div className="map-wrap">
+        <div ref={mapContainer} className="map" />
+    </div>
+    );
 }
